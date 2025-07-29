d5c60ac9ff02496368222159483b81a38ee2862b
Kaikina <finalfantasydeadcrisis@hotmail.fr>
2025-07-29 10:31:01 +0200

    fix(filemanager): Instantiate correct Kernel class

--- admin-dev/filemanager/config/config.php
+++ admin-dev/filemanager/config/config.php
@@ -40,7 +40,7 @@ global $kernel;
 if (!$kernel) {
     require_once _PS_ROOT_DIR_ . '/app/AdminKernel.php';
 
-    $kernel = new AppKernel(
+    $kernel = new AdminKernel(
         _PS_ENV_,
         _PS_MODE_DEV_
     );
