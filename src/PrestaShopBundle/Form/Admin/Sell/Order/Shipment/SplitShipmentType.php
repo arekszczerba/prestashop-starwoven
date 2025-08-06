aa50fdc87eeb31139331fadfa9af7b73741b9e1c
Alexis Guyomar <alexis.guyomar@prestashop.com>
2025-08-06 14:54:01 +0200

    fix: cs fixer

--- src/PrestaShopBundle/Form/Admin/Sell/Order/Shipment/SplitShipmentType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Order/Shipment/SplitShipmentType.php
@@ -40,8 +40,7 @@ class SplitShipmentType extends AbstractType
     public function __construct(
         private readonly AvailableCarriersForShipmentChoiceProvider $availableCarriersForShipmentChoiceProvider,
         private readonly TranslatorInterface $translator
-        )
-    {
+    ) {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
